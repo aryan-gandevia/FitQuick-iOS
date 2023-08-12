6a846925729b22e0dcee6ae335f87da3416ec69c
Aryan Gandevia <aryan.gandevia@gmail.com>
2023-08-12 17:58:18 -0400

    Initial Commit

---
+++ FitQuick/FitQuickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuickApp.swift
+//  FitQuick
+//
+//  Created by Aryan Gandevia on 2023-08-12.
+//
+
+import SwiftUI
+
+@main
+struct FitQuickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
